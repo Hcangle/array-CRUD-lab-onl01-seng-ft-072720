aa44d9502927e832920079fdd4e994666e14d5b0
Hannah Angle <hannahcangle@gmail.com>
2020-08-01 02:33:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,14 @@
 def create_an_empty_array
-  
+  [ ]
 end
 
 def create_an_array
-  
+   famous_cats = ["lil' bub", "grumpy cat", "Maru", "Tony"]
 end
-
+   
 def add_element_to_end_of_array(array, element)
-  
+ 
+ 
 end
 
 def add_element_to_start_of_array(array, element)
